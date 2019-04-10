d31bee23266934218a49b41617a3fbe25a2138ed
Juan <juanfob@gmail.com>
2019-04-10 16:06:14 +0200

    Subida hasta unidad 7 finalizada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        if (request()->has('empty')){
+            $users = [];
+        }else {
+            $users = [
+                'Joel',
+                'Ellie',
+                'Tess',
+                'Tommy',
+                'Bill',
+                '<script>alert("Clicker")</script>'
+            ];
+        }
+        return view('users')->with([
+            'users' => $users,
+            'title' => 'Listado de Usuarios'
+        ]);
+    }
+
+    public function show($id)
+    {
+        return view('show')->with([
+            'id' => $id
+        ]);
+
+    }
+
+    public function create()
+    {
+        return 'Crear nuevo usuario';
+    }
 }
--- tests/Feature/UserModuleTest.php
+++ tests/Feature/UserModuleTest.php
@@ -13,11 +13,25 @@ class UserModuleTest extends TestCase
      *
      * @return void
      */
-    function test_it_loads_the_users_list_page()
+    function test_it_shows_the_users_list()
     {
         $this->get('/usuarios')
             ->assertStatus(200)
-            ->assertSee('Usuarios');
+            ->assertSee('Listado de Usuarios')
+            ->assertSee('Joel')
+            ->assertSee('Ellie');
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    function test_it_shows_a_default_message_if_the_users_list_is_empty()
+    {
+        $this->get('/usuarios?empty')
+            ->assertStatus(200)
+            ->assertSee('No hay usuarios registrados');
     }
 
     function test_it_loads_the_user_details_page()
