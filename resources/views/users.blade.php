4d350fac5df8ab836b3d8b987b4970ed077b176c
Juan Fermin <juanfob.dev@gmail.com>
2020-01-28 15:55:03 +0100

    Hasta leccion 8 minuto 7

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,27 +1,19 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listado de usuarios - Styde.net</title>
-</head>
-<body>
-    <h1>{{ $title }}</h1>
-
-    <hr>
-
-    @if(! empty($users))
-        <ul>
-            @foreach ($users as $user)
-                <li>{{ $user }} </li>
-            @endforeach
-        </ul>
-    @else
-        <p>No hay usuarios registrados</p>
-    @endif
+@include('header')
+    <div class="row mt-5">
+      <div class="col-8">
+        <h1>{{ $title }}</h1>
 
 
-</body>
-</html>
+        @if(! empty($users))
+            <ul>
+                @foreach ($users as $user)
+                    <li>{{ $user }} </li>
+                @endforeach
+            </ul>
+        @else
+            <p>No hay usuarios registrados</p>
+        @endif
+    </div>
+        @include('sidebar')
+    </div>
+@include('footer')
